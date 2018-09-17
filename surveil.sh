6291603045579228fc6eb7706705504ddb2c2f42
Morten W. Petersen <morphex@gmail.com>
2018-09-17 09:47:29 +0000

    Various cleanups, adding Cron shell script

---
+++ surveil.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Shell script to run surveil.py from Cron
+#
+# For example, enter
+#
+# @reboot /root/surveil/surveil.sh email-address
+#
+PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export PATH
+
+cd /root/surveil
+python3 ./surveil.py $1 &>surveil.log
+
+
+
